e823660c4cdf23202027b5d71f26363a65a992ef
Katie Toney <kathryn.toney@gmail.com>
2023-05-19 12:15:29 -0400

    added routes

--- app.js
+++ app.js
@@ -23,15 +23,15 @@ app.use(passport.initialize());
 app.use(passport.session())
 
 //routes 
-app.use(require('./routes/index.js'))
-app.use(require('./routes/login.js'))
-app.use(require('./routes/registration.js'))
-app.use(require('./routes/dashboard.js')) // manage your profile / settings
-app.use(require('./routes/aboutus.js'))
-app.use(require('./routes/contactus.js'))
-app.use(require('./routes/messages.js')) // DM with another dog
+// app.use(require('./routes/index.js'))
+// app.use(require('./routes/login.js'))
+// app.use(require('./routes/registration.js'))
+// app.use(require('./routes/dashboard.js')) // manage your profile / settings
+// app.use(require('./routes/aboutus.js'))
+// app.use(require('./routes/contactus.js'))
+// app.use(require('./routes/messages.js')) // DM with another dog
 app.use(require('./routes/profile.js')) // view another dog's profile
-app.use(require('./routes/search.js')) // view another dog's profile
+// app.use(require('./routes/search.js')) // view another dog's profile
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
